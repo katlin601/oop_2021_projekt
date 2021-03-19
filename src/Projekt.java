6641e0e2ba6ca2577c86b7d2ba1a3a512ac31f26
m2tth <matthiasraukas@gmail.com>
2021-03-19 14:28:46 +0200

    Projekt

--- src/Projekt.java
+++ src/Projekt.java
@@ -1,5 +1,15 @@
 public class Projekt {
 
+    String projekt;
+
+    public String getProjekt() {
+        return projekt;
+    }
+
+    public void setProjekt(String projekt) {
+        this.projekt = projekt;
+    }
+
     public static void main(String[] args) {
         System.out.println("See on meie tore projekt siis");
     }
